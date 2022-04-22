beaba5dfe27ce02e98986814e58b09018e5d5ef2
danixuz <danixuz@icloud.com>
2022-04-23 01:43:07 +0300

    Initial Commit

---
+++ appTest/appTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  appTestApp.swift
+//  appTest
+//
+//  Created by Daniel Spalek on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct appTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
